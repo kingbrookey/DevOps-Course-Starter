eac2973937cdebd9220c5268b10984f896c892b9
kingbrookey <124798759+kingbrookey@users.noreply.github.com>
2023-06-13 13:23:52 +0000

    modified:   todo_app/app.py modified:   todo_app/data/session_items.py modified:   todo_app/templates/index.html

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
-
+from flask import render_template
+from flask import request
+from flask import redirect
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_item
+from todo_app.data.session_items import add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +13,15 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items=items)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    item = request.form.get('item')
+    items = add_item(item)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
