e34b4bfa1088aae9bcd26905bf2bd981fb086e66
kingbrookey <124798759+kingbrookey@users.noreply.github.com>
2023-07-18 10:24:55 +0000

    Pending changes exported from your codespace

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,25 +1,45 @@
-from flask import Flask
-from flask import render_template, request, redirect
+import requests
+import os
+from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
-from todo_app.data.session_items import get_item
-from todo_app.data.session_items import add_item
+from todo_app.data.session_items import get_items, get_item, add_item
+from todo_app.data.trello_items import fetch_todo_items, find_list_id_by_name, create_todo_card
 
+    
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
+# Update the function that handles the GET request for fetching todo items
 @app.route('/')
 def index():
-    items = get_items()
-    return render_template('index.html', items=items)
+    # Get Board ID, API key and API token from env file
+    board_id = os.getenv('BOARD_ID')
+    api_key = os.getenv('API_KEY')
+    api_token = os.getenv('API_TOKEN')
+    
+
+    # Use the fetch_todo_items function from trello_items module
+    todo_items = fetch_todo_items(board_id, api_key, api_token)
+    
+    return render_template('index.html', items=todo_items)
 
 
-@app.route('/add', methods=['POST'])
-def add():
-    item = request.form.get('item')
-    items = add_item(item)
-    return redirect('/')
+
+@app.route('/add')
+def add_new_card():
+    # Get Board ID, API key and API token from env file
+    board_id = os.getenv('BOARD_ID')
+    api_key = os.getenv('API_KEY')
+    api_token = os.getenv('API_TOKEN')
+    board_data = fetch_todo_items(board_id, api_key, api_token)
+    list_id = find_list_id_by_name("To Do",board_data)
+    card_name = "To Do"
+        
+  # Call the function to create a new card
+    card_id = create_todo_card(board_id, list_id, card_name, api_key, api_token)
+    
+    return render_template('index.html', items=board_data)
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
---
+++ todo_app/data/trello_items.py
@@ -0,0 +1,38 @@
+# trello_items.py
+
+import requests
+
+def fetch_todo_items(board_id, api_key, api_token):
+    url = f"https://api.trello.com/1/boards/{board_id}/lists?cards=open&key={api_key}&token={api_token}"
+    response = requests.get(url)
+    data = response.json()
+    
+    #todo_items = []
+    trello_to_do = []
+    for item in data:
+        trello_to_do.append({'id': item['id'], 'name': item['name']})
+        print(trello_to_do)
+        #cards = item["cards"]
+        #todo_items.extend(cards)
+    
+    return trello_to_do
+
+
+def create_todo_card(board_id, list_id, card_name, api_key, api_token):
+    url = f"https://api.trello.com/1/cards?key={api_key}&token={api_token}"
+    params = {
+        'idList': list_id,
+        'name': card_name,
+        'idBoard': board_id
+    }
+    response = requests.post(url, params=params)
+    data = response.json()
+    
+    return data.get('id')  # Return the ID of the created card
+
+
+def find_list_id_by_name(name, data):
+    for item in data:
+        if item['name'] == name:
+            return item['id']
+    return None
